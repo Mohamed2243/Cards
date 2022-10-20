3de7fe6acb6bffc363bde3a352255facb1b48626
Mohamed Elenaby <mohamedelnby48@gmail.com>
2022-10-20 15:24:08 +0200

    Add Back End Project

---
+++ Back/CardApi/Data/DataCard.cs
@@ -0,0 +1,17 @@
+﻿using CardApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CardApi.Data
+{
+    public class CardsDbContext : DbContext
+    {
+        public CardsDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        //Dbset
+        public DbSet<Card> Cards { set; get; }
+
+
+    }
+}
